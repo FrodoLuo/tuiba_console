a05c6272653595cf0ca3e2f352776697debf5680
FrodoLuo <359303713@qq.com>
2017-09-06 09:16:13 +0800

    校验完成 接口到位之后可以对接

--- src/components/SignUp/Form.vue
+++ src/components/SignUp/Form.vue
@@ -4,23 +4,23 @@
       主办方信息
     </div>
     <el-row type="flex" class="background">
-      <el-form :model="form" labelWidth="150px">
-        <el-form-item label="主办方名称">
-          <el-input v-model="form.name"></el-input>
+      <el-form :model="signForm" ref="signForm" :rules="rules" labelWidth="150px">
+        <el-form-item label="主办方名称" required="" prop="name">
+          <el-input v-model="signForm.name"></el-input>
         </el-form-item>
-        <el-form-item label="主办方类别">
-          <el-select v-model="form.type" value="">
+        <el-form-item label="主办方类别" required="" prop="type">
+          <el-select v-model="signForm.type" value="">
             <el-option label="个人" value="0">个人</el-option>
             <el-option value="社团">社团</el-option>
             <el-option value="学校">学校</el-option>
             <el-option value="企业">企业</el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="负责人ID">
-          <el-input v-model="form.leaderID"></el-input>
+        <el-form-item label="负责人ID" required="" prop="leaderID">
+          <el-input v-model="signForm.leaderID"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button @click="this.onSubmit">提交</el-button>
+          <el-button @click="onSub('signForm')">提交</el-button>
         </el-form-item>
       </el-form>
     </el-row>
@@ -30,24 +30,45 @@
   import ElementUI from 'element-ui'
   import Vue from 'vue'
   import 'element-ui/lib/theme-default/index.css'
-  import ElButton from "../../../node_modules/element-ui/packages/button/src/button.vue";
+  import ElButton from "../../../node_modules/element-ui/packages/button/src/button.vue"
+  import * as service from '../../service/services'
 
   Vue.use(ElementUI)
   export default {
     components: {ElButton},
-    name: 'form',
+    name: 'signForm',
     data() {
       return {
-        form: {
+        signForm: {
           name: '',
           type: '',
           leaderID: ''
+        },
+        rules: {
+          name: [
+            { required: true, message: '主办方姓名不能为空', trigger: 'change' }
+          ],
+          type: [
+            { required: true, message: '主办方类别不能为空', trigger: 'change' }
+          ],
+          leaderID: [
+            { required: true, message: '负责人ID不能为空', trigger: 'change' }
+          ]
         }
       }
     },
     methods: {
-      onSubmit() {
-        console.log(this.form);
+      onSub(formName) {
+        console.log(formName);
+        console.log(this);
+        this.$refs[formName].validate((valid) =>{
+          if (valid) {
+            service.signUpParty(this.signForm)
+          } else {
+            return false;
+          }
+        })
+
       }
     }
   }
--- src/service/services.js
+++ src/service/services.js
@@ -3,10 +3,13 @@ import qs from 'qs'
 
 axios.defaults.timeout=500;
 axios.defaults.headers.post['Content-Type']= 'text/plain;charset=utf-8';
-axios.defaults.baseURL='localhost:8080';
+axios.defaults.baseURL='127.0.0.1:8080';
 
 export function signUpParty(form){
-  axios.get('',{
-
+  console.log(form);
+  axios.get('s',{
+    param: {
+      wd: form.name
+    }
   })
 }
